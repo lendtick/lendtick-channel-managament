191c18c9455e9cfd5f7b1bef916fcc630405efc3
Tupay <lutfi@mhs.ubharajaya.ac.id>
2018-10-14 17:36:06 +0700

    bugsfix rewind

--- app/Http/Controllers/ChannelController.php
+++ app/Http/Controllers/ChannelController.php
@@ -19,20 +19,18 @@ class ChannelController  extends Controller
     
 
     public function index(){
-        // print_r()
-        phpinfo();
-        // try{
-        //     $status   = 1;
-        //     $httpcode = 200;
-        //     $data     = $this->ChannelRepo->all();
-        //     $errorMsg = null;
-        // }catch(\Exception $e){
-        //     $status   = 0;
-        //     $httpcode = 400;
-        //     $data     = null;
-        //     $errorMsg = $e->getMessage();
-        // }
-        // return response()->json(Api::format($status, $data, $errorMsg), $httpcode);
+        try{
+            $status   = 1;
+            $httpcode = 200;
+            $data     = $this->ChannelRepo->all();
+            $errorMsg = null;
+        }catch(\Exception $e){
+            $status   = 0;
+            $httpcode = 400;
+            $data     = null;
+            $errorMsg = $e->getMessage();
+        }
+        return response()->json(Api::format($status, $data, $errorMsg), $httpcode);
     } 
 
 }
